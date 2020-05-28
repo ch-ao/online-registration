c81fa8ce1bedc63b14f70bfe636ea2c22a00a741
Chao Zhang <raidenchao@gmail.com>
2020-05-28 06:59:11 -0700

    update with html template

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import NetlifyForm from 'react-netlify-form';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
       width: '100%',
-    },
+    }
   },
 }));
 
@@ -21,40 +20,16 @@ export default function BasicTextFields() {
     <div>
       <Typography color='primary' variant='h5' align='center'>知行教育 - 在线报名表</Typography>
 
-      <NetlifyForm name='Registration Form'> 
-        {({ loading, error, success }) => (
-          <div>
-            {loading &&
-              <div>Loading...</div>
-            }
-            {error &&
-              <div>Your information was not sent. Please try again later.</div>
-            }
-            {success &&
-              <div>Thank you for contacting us!</div>
-            }
-            {!loading && !success &&
-              <div className={classes.root}>
-                <TextField id="nameParent" label="家长姓名" required='true'  variant="outlined" />
-                <TextField id="phone" label="联系电话" variant="outlined" />
-                <TextField id="wechat" label="微信" required='true'  variant="outlined" />
-                <TextField id="nameStudent" label="学生姓名" required='true' variant="outlined" />
-                <TextField id="grade" label="学生年级" required='true'  variant="outlined" />
-                <TextField id="course" label="报名课程" required='true'  variant="outlined" />
-                <Button type='submit' className={classes.button}  variant="contained" color="primary">提 交</Button>
-              </div>
-            }
-            
-          </div>
-        )}
-
-      </NetlifyForm>
-
-      {/* <form name='registration' method="post" netlify className={classes.root} autoComplete="off">
-     
-      </form> */}
+      <form name='contact' method="post" netlify className={classes.root} autoComplete="off">
+        <TextField id="nameParent" label="家长姓名" required='true'  variant="outlined" />
+        <TextField id="phone" label="联系电话" variant="outlined" />
+        <TextField id="wechat" label="微信" required='true'  variant="outlined" />
+        <TextField id="nameStudent" label="学生姓名" required='true' variant="outlined" />
+        <TextField id="grade" label="学生年级" required='true'  variant="outlined" />
+        <TextField id="course" label="报名课程" required='true'  variant="outlined" />
+        <Button type='submit' className={classes.button}  variant="contained" color="primary">提 交</Button>
+      </form>
     </div>
-    
-
+  
   );
 }
